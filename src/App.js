5332628d508eb02fdda17a1863b3c500b0f832a5
Hans-Jakob Brandt <hans-jakob.brandt@MWNOOSLMACLT434.local>
2018-09-18 08:45:41 +0200

    Import tabler from npm

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import "./App.css";
 
 import Header from "./components/Header/Header";
 
+import { Card, Button } from "tabler-react";
+
+//CSS
+import "tabler-react/dist/Tabler.css";
+
 class App extends Component {
   render() {
     return (
@@ -11,6 +16,14 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Card>
+          <Card.Header>
+            <Card.Title>Card Title</Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <Button color="primary">A Button</Button>
+          </Card.Body>
+        </Card>
       </div>
     );
   }
