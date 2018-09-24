2a81a58d286c4f721c59f014301ec7e4b579743c
Hans-Jakob Brandt <hans-jakob.brandt@MWNOOSLMACLT434.local>
2018-09-24 14:49:54 +0200

    add the d3 library

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -8,122 +8,120 @@ export default class Dashboard extends Component {
       <Page.Content>
         <Page.Title>Dashbord</Page.Title>
         <Grid.Row>
-          <Grid.Col width={12}>
-            <ht className="card">
-              <Card.Header>
-                <Card.Title>Grensesvingen 26</Card.Title>
-              </Card.Header>
-              <Card.Body>
-                <Form.Group label="Vifter">
-                  <Form.ColorCheck>
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="red" />
-                    <Form.ColorCheckItem color="orange" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                  </Form.ColorCheck>
-                </Form.Group>
-                <Form.Group label="Vendere">
-                  <Form.ColorCheck>
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="red" />
-                    <Form.ColorCheckItem color="orange" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                  </Form.ColorCheck>
-                </Form.Group>
-                <Form.Group label="Pumper">
-                  <Form.ColorCheck>
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="red" />
-                    <Form.ColorCheckItem color="orange" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                  </Form.ColorCheck>
-                </Form.Group>
-                <Form.Group label="Viftevakt">
-                  <Form.ColorCheck>
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="red" />
-                    <Form.ColorCheckItem color="orange" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                  </Form.ColorCheck>
-                </Form.Group>
-                <Form.Group label="Varmekabel">
-                  <Form.ColorCheck>
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="red" />
-                    <Form.ColorCheckItem color="orange" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                  </Form.ColorCheck>
-                </Form.Group>
-                <Form.Group label="Spjeld">
-                  <Form.ColorCheck>
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="red" />
-                    <Form.ColorCheckItem color="orange" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                    <Form.ColorCheckItem color="green" />
-                  </Form.ColorCheck>
-                </Form.Group>
-              </Card.Body>
-            </ht>
+          <Grid.Col className="card" width={12}>
+            <Card.Header>
+              <Card.Title>Grensesvingen 26</Card.Title>
+            </Card.Header>
+            <Card.Body>
+              <Form.Group label="Vifter">
+                <Form.ColorCheck>
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="red" />
+                  <Form.ColorCheckItem color="orange" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                </Form.ColorCheck>
+              </Form.Group>
+              <Form.Group label="Vendere">
+                <Form.ColorCheck>
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="red" />
+                  <Form.ColorCheckItem color="orange" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                </Form.ColorCheck>
+              </Form.Group>
+              <Form.Group label="Pumper">
+                <Form.ColorCheck>
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="red" />
+                  <Form.ColorCheckItem color="orange" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                </Form.ColorCheck>
+              </Form.Group>
+              <Form.Group label="Viftevakt">
+                <Form.ColorCheck>
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="red" />
+                  <Form.ColorCheckItem color="orange" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                </Form.ColorCheck>
+              </Form.Group>
+              <Form.Group label="Varmekabel">
+                <Form.ColorCheck>
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="red" />
+                  <Form.ColorCheckItem color="orange" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                </Form.ColorCheck>
+              </Form.Group>
+              <Form.Group label="Spjeld">
+                <Form.ColorCheck>
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="red" />
+                  <Form.ColorCheckItem color="orange" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                  <Form.ColorCheckItem color="green" />
+                </Form.ColorCheck>
+              </Form.Group>
+            </Card.Body>
           </Grid.Col>
         </Grid.Row>
       </Page.Content>
--- src/components/details/DetailsContainer.jsx
+++ src/components/details/DetailsContainer.jsx
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-
+import Details from './Details';
 //Tabler
-import { Grid } from 'tabler-react';
+import { Grid, Card } from 'tabler-react';
 
 export default class DetailsContainer extends Component {
   render() {
     return (
-      <Grid.Col width={6}>
-        <div>DetailsContainer</div>
+      <Grid.Col className="card" width={6}>
+        <Card.Header>
+          <Card.Title>Sensor Name</Card.Title>
+        </Card.Header>
+        <Card.Body>
+          <Details />
+        </Card.Body>
       </Grid.Col>
     );
   }
